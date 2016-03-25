ead3a62dd2f9dceff93887d50dcc676499ce5f79
Mika Turunen <mika.turunen@ymail.com>
2016-03-25 17:55:13 +0200

    Additional unused, dead code removed.

--- slushfile.js
+++ slushfile.js
@@ -17,41 +17,6 @@ var _ = require("underscore.string");
 var inquirer = require("inquirer");
 var path = require("path");
 
-function format(string) {
-    var username = string.toLowerCase();
-    return username.replace(/\s/g, "");
-}
-
-var defaults = (function () {
-    var workingDirName = path.basename(process.cwd());
-    var homeDir;
-	var osUserName;
-	var configFile;
-	var user;
-
-    if (process.platform === "win32") {
-        homeDir = process.env.USERPROFILE;
-        osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
-    } else {
-        homeDir = process.env.HOME || process.env.HOMEPATH;
-        osUserName = homeDir && homeDir.split("/").pop() || "root";
-    }
-
-    configFile = path.join(homeDir, ".gitconfig");
-    user = {};
-
-    if (require("fs").existsSync(configFile)) {
-        user = require("iniparser").parseSync(configFile).user;
-    }
-
-    return {
-        appName: workingDirName,
-        userName: osUserName || format(user.name || ""),
-        authorName: user.name || "",
-        authorEmail: user.email || ""
-    };
-})();
-
 gulp.task("default", function (done) {
     var prompts = [{
         name: "componentNameCamelCase",
